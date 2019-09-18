f74a83efbe8712df554135f6d234ffd83c1f8c5f
Hill <hill@kimo.com>
2019-09-18 10:20:58 +0800

    improve preview.sh

--- preview.sh
+++ preview.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+ENV="${DIR}/.env"
+
+if [ -e "${ENV}" ]; then
+  MDBOOK_SRC=$(awk -F "=" '/^MDBOOK_SRC/ {print $2}' $ENV)
+  if [ ! -z "${MDBOOK_SRC}" ]; then
+    MDBOOK_SRC=$(pwd)/$MDBOOK_SRC
+  fi
+  CONTAINER_NAME=$(awk -F "=" '/^CONTAINER_NAME/ {print $2}' $ENV)
+  PORT=$(awk -F "=" '/^PORT/ {print $2}' $ENV)
+  WS_PORT=$(awk -F "=" '/^WS_PORT/ {print $2}' $ENV)
+fi
+
+MDBOOK_SRC=${MDBOOK_SRC:-$(pwd)}
+CONTAINER_NAME=${CONTAINER_NAME:-mdbook}
+PORT=${PORT:-3000}
+WS_PORT=${WS_PORT:-3001}
+
+# echo $MDBOOK_SRC
+# echo $CONTAINER_NAME
+# echo $PORT
+# echo $WS_PORT
+# exit;
+
 start() {
   stop
-  docker run -d -p 3000:3000 -p 3001:3001 -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$(pwd)/mdbook-demo/src:/mdbook/src" --name mdbook hillliu/mdbook serve -n 0.0.0.0 
+  docker run -d -p ${PORT}:3000 -p ${WS_PORT}:3001 -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "${MDBOOK_SRC}:/mdbook/src" --name ${CONTAINER_NAME} hillliu/mdbook serve -n 0.0.0.0 
 }
 
 stop() {
-  docker stop mdbook
-  docker rm mdbook
+  docker stop ${CONTAINER_NAME}
+  docker rm ${CONTAINER_NAME}
+}
+
+status() {
+  docker ps -f name=${CONTAINER_NAME}
 }
 
 logs() {
-  docker logs -f mdbook
+  docker logs -f ${CONTAINER_NAME}
 }
 
 case "$1" in
@@ -22,11 +50,14 @@ case "$1" in
   stop)
     stop 
     ;;
+  status)
+    status 
+    ;;
   logs)
     logs 
     ;;
   *)
-    echo "$0 [start|stop|logs]" 
+    echo "$0 [start|stop|status|logs]" 
     exit
 esac
 
