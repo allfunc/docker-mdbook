9f8b68b31cde8e236a247fe0a8afb5934008a10b
Allen Liu <allen@omnibpm.com>
2019-09-24 12:53:37 +0800

    Modify mdbook src path to absolute path

--- preview.sh
+++ preview.sh
@@ -6,7 +6,7 @@ ENV="${DIR}/.env"
 if [ -e "${ENV}" ]; then
   MDBOOK_SRC=$(awk -F "=" '/^MDBOOK_SRC/ {print $2}' $ENV)
   if [ ! -z "${MDBOOK_SRC}" ]; then
-    MDBOOK_SRC=$(pwd)/$MDBOOK_SRC
+    MDBOOK_SRC=/$MDBOOK_SRC
   fi
   CONTAINER_NAME=$(awk -F "=" '/^CONTAINER_NAME/ {print $2}' $ENV)
   PORT=$(awk -F "=" '/^PORT/ {print $2}' $ENV)
@@ -27,7 +27,7 @@ WS_PORT=${WS_PORT:-$((PORT+1))}
 start() {
   stop
   docker run -d -p ${PORT}:3000 -p ${WS_PORT}:3001 -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "${MDBOOK_SRC}:/mdbook/src" --name ${CONTAINER_NAME} hillliu/mdbook serve -n 0.0.0.0 
+    -v "${MDBOOK_SRC}:/mdbook/src" --name ${CONTAINER_NAME} hillliu/mdbook serve -n 0.0.0.0
 }
 
 stop() {
@@ -48,17 +48,17 @@ case "$1" in
     start
     ;;
   stop)
-    stop 
+    stop
     ;;
   status)
-    status 
+    status
     ;;
   logs)
-    logs 
+    logs
     ;;
   *)
     echo "$0 [start|stop|status|logs]"
     exit
 esac
 
-exit $?  
+exit $?
