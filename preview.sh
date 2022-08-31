3f0f31a02ee1853ae272ad4ce540c289776cbc24
Hill <hill@kimo.com>
2022-09-01 01:31:47 +0800

    fixed pwd not work on all os

--- preview.sh
+++ preview.sh
@@ -6,14 +6,14 @@ ENV="${DIR}/.env"
 if [ -e "${ENV}" ]; then
   MDBOOK_SRC=$(awk -F "=" '/^MDBOOK_SRC/ {print $2}' $ENV)
   if [ ! -z "${MDBOOK_SRC}" ]; then
-    MDBOOK_SRC=$(pwd)/$MDBOOK_SRC
+    MDBOOK_SRC=$DIR/$MDBOOK_SRC
   fi
   CONTAINER_NAME=$(awk -F "=" '/^CONTAINER_NAME/ {print $2}' $ENV)
   PORT=$(awk -F "=" '/^PORT/ {print $2}' $ENV)
   WS_PORT=$(awk -F "=" '/^WS_PORT/ {print $2}' $ENV)
 fi
 
-MDBOOK_SRC=${MDBOOK_SRC:-$(pwd)}
+MDBOOK_SRC=${MDBOOK_SRC:-$DIR}
 CONTAINER_NAME=${CONTAINER_NAME:-mdbook}
 PORT=${PORT:-3000}
 WS_PORT=${WS_PORT:-$((PORT+1))}
